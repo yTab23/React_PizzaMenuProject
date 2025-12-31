1fc8804f964c696339b745a154bbca0f01644c31
Yuki Tabata <95216071+yTab23@users.noreply.github.com>
2025-12-31 21:52:46 +1100

    Basic functionality of menu

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import "./App.css";
+import React from "react";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        <Menu />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza</h1>;
+}
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} - We're currently open</footer>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="../assets/pizzas/focaccia.jpg" />
+      <h2>Pizza</h2>
+      <p>Spinach salamino</p>
+    </div>
+  );
+}
+export default App;
