e6eba5f81e53c1c192757c73be99485a231516a1
Yuki Tabata <95216071+yTab23@users.noreply.github.com>
2026-01-05 14:31:08 +1100

    Prevent rendering pizza object in mneu if it is sold out

--- src/App.jsx
+++ src/App.jsx
@@ -110,6 +110,8 @@ function Footer() {
 }
 
 function Pizza(props) {
+  if (props.pizzaObj.soldOut) return null;
+
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
